84fb3f041116947155a312b7c810ea5d9e36f9f6
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-06-10 00:47:17 +0700

    Feat: Change Staking position strucyt, Add fn getStakingAmount()

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Elrond-staking-contract
-staking Contract 
+
+staking Contract
 Elrond blockchain - MultiversX network
 
 Contract address: erd1qqqqqqqqqqqqqpgqtpz4yht3ern76l8333h22rk95dj7pdk2f9dqpl3qjs
@@ -12,12 +13,12 @@ Clone the repo and go in the directory
 
 `git clone https://github.com/gianmarcoalarcon/Elrond-staking-contract.git`
 
-- Query the stakers list 
+- Query the stakers list
 
 `mxpy --verbose contract query erd1qqqqqqqqqqqqqpgqtpz4yht3ern76l8333h22rk95dj7pdk2f9dqpl3qjs \
     --proxy=https://devnet-gateway.multiversx.com \
     --function="getStakedAddresses"`
-    
+
 - Call the stake function
 
 `mxpy --verbose contract call erd1qqqqqqqqqqqqqpgqtpz4yht3ern76l8333h22rk95dj7pdk2f9dqpl3qjs \
@@ -26,15 +27,14 @@ Clone the repo and go in the directory
     --gas-limit=10000000 \
     --value=1000000000000 \
     --function="stake"`
-  
+
 - Calculate the rewards per user
 
 `mxpy --verbose contract query erd1qqqqqqqqqqqqqpgqtpz4yht3ern76l8333h22rk95dj7pdk2f9dqpl3qjs \
     --proxy=https://devnet-gateway.multiversx.com \
     --function="calculateRewardsForUser" \
     --arguments {UserAddress}`
-    
-    
+
 - Call the unstake function
 
 `mxpy --verbose contract call erd1qqqqqqqqqqqqqpgqtpz4yht3ern76l8333h22rk95dj7pdk2f9dqpl3qjs \
@@ -43,7 +43,32 @@ Clone the repo and go in the directory
     --gas-limit=10000000 \
     --function="unstake" \
     --arguments 500000`
-    
-## Test locally 
 
-  `cargo test --test rust_test`
+## Test locally
+
+`cargo test --test rust_test`
+
+## Deploy your own contract
+
+`mxpy --verbose contract deploy --bytecode=/home/gianm/rust-web3/staking-contract/output/staking-contract.wasm \
+    --recall-nonce --pem=/home/gianm/rust-web3/staking-contract/walletKey.pem \
+    --gas-limit=20000000 \
+    --send --outfile="deploy-devnet.interaction.json" --wait-result \
+    --proxy=https://devnet-gateway.multiversx.com --chain=D \
+		--arguments 300`
+
+mxpy --verbose contract query erd1qqqqqqqqqqqqqpgq2us4uxcz4u6l3kvpyl0f0gv37pwq3vttf9dqsyzlp9 \
+ --proxy=https://devnet-gateway.multiversx.com \
+ --function="getStakedAddresses"
+
+    mxpy --verbose contract query erd1qqqqqqqqqqqqqpgq2us4uxcz4u6l3kvpyl0f0gv37pwq3vttf9dqsyzlp9 \
+    --proxy=https://devnet-gateway.multiversx.com \
+    --function="getStakingAmount" \
+    --arguments erd1duy4zc20pdy666chknywet227n9yjmc6uvfgjcy5yvcxncddf9dqxqwzjl
+
+mxpy --verbose contract call erd1qqqqqqqqqqqqqpgq2us4uxcz4u6l3kvpyl0f0gv37pwq3vttf9dqsyzlp9 \
+ --proxy=https://devnet-gateway.multiversx.com --chain=D \
+ --send --recall-nonce --pem=/home/gianm/rust-web3/staking-contract//walletKey.pem \
+ --gas-limit=10000000 \
+ --value=1000000000000000 \
+ --function="stake"
--- src/staking.rs
+++ src/staking.rs
@@ -1,6 +1,5 @@
 #![no_std]
 
-
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
@@ -20,30 +19,29 @@ pub trait StakingContract {
         self.base_distribution().set(base_distribution);
     }
 
-		#[payable("EGLD")]
+    #[payable("EGLD")]
     #[endpoint]
     fn stake(&self) {
         let payment_amount = self.call_value().egld_value().clone_value();
         require!(payment_amount > 0, "Must pay more than 0");
 
         let caller = self.blockchain().get_caller();
-				let stake_mapper = self.staking_position(&caller);
+        let stake_mapper_amount = self.staking_amount(&caller);
+        let stake_mapper_block = self.staking_block(&caller);
 
         let is_new_user = self.staked_addresses().insert(caller.clone());
-        let mut staking_pos = if !is_new_user {
-            stake_mapper.get()
+        let (mut staking_amount, mut staking_block) = if !is_new_user {
+            (stake_mapper_amount.get(), stake_mapper_block.get())
         } else {
             let current_block = self.blockchain().get_block_epoch();
-            StakingPosition {
-                stake_amount: BigUint::zero(),
-                last_action_block: current_block,
-            }
+            (BigUint::zero(), current_block)
         };
 
-        self.claim_rewards_for_user(&caller, &mut staking_pos);
-        staking_pos.stake_amount += payment_amount;
+        self.claim_rewards_for_user(&caller, &staking_amount, &mut staking_block);
+        staking_amount += payment_amount;
 
-        stake_mapper.set(&staking_pos);
+        stake_mapper_amount.set(&staking_amount);
+        stake_mapper_block.set(&staking_block);
     }
 
     #[endpoint]
@@ -51,25 +49,29 @@ pub trait StakingContract {
         let caller = self.blockchain().get_caller();
         self.require_user_staked(&caller);
 
-        let stake_mapper = self.staking_position(&caller);
-        let mut staking_pos = stake_mapper.get();
+        let stake_mapper_amount = self.staking_amount(&caller);
+        let stake_mapper_block = self.staking_block(&caller);
+        let mut staking_amount = stake_mapper_amount.get();
+        let mut staking_block = stake_mapper_block.get();
 
         let unstake_amount = match opt_unstake_amount {
             OptionalValue::Some(amt) => amt,
-            OptionalValue::None => staking_pos.stake_amount.clone(),
+            OptionalValue::None => staking_amount.clone(),
         };
         require!(
-            unstake_amount > 0 && unstake_amount <= staking_pos.stake_amount,
+            unstake_amount > 0 && unstake_amount <= staking_amount,
             "Invalid unstake amount"
         );
 
-        self.claim_rewards_for_user(&caller, &mut staking_pos);
-        staking_pos.stake_amount -= &unstake_amount;
+        self.claim_rewards_for_user(&caller, &staking_amount, &mut staking_block);
+        staking_amount -= &unstake_amount;
 
-        if staking_pos.stake_amount > 0 {
-            stake_mapper.set(&staking_pos);
+        if staking_amount > 0 {
+            stake_mapper_amount.set(&staking_amount);
+            stake_mapper_block.set(&staking_block);
         } else {
-            stake_mapper.clear();
+            stake_mapper_amount.clear();
+            stake_mapper_block.clear();
             self.staked_addresses().swap_remove(&caller);
         }
 
@@ -81,11 +83,14 @@ pub trait StakingContract {
         let caller = self.blockchain().get_caller();
         self.require_user_staked(&caller);
 
-        let stake_mapper = self.staking_position(&caller);
-        let mut staking_pos = stake_mapper.get();
-        self.claim_rewards_for_user(&caller, &mut staking_pos);
+        let stake_mapper_amount = self.staking_amount(&caller);
+        let stake_mapper_block = self.staking_block(&caller);
+        let staking_amount = stake_mapper_amount.get();
+        let mut staking_block = stake_mapper_block.get();
+        self.claim_rewards_for_user(&caller, &staking_amount, &mut staking_block);
 
-        stake_mapper.set(&staking_pos);
+        stake_mapper_amount.set(&staking_amount);
+        stake_mapper_block.set(&staking_block);
     }
 
     fn require_user_staked(&self, user: &ManagedAddress) {
@@ -94,47 +99,57 @@ pub trait StakingContract {
     fn claim_rewards_for_user(
         &self,
         user: &ManagedAddress,
-        staking_pos: &mut StakingPosition<Self::Api>,
+        staking_amount: &BigUint,
+        staking_block: &mut u64,
     ) {
-        let reward_amount = self.calculate_rewards(staking_pos);
+        let reward_amount = self.calculate_rewards(staking_amount, staking_block);
         let current_block = self.blockchain().get_block_nonce();
-        staking_pos.last_action_block = current_block;
+        *staking_block = current_block;
 
         if reward_amount > 0 {
             self.send().direct_egld(user, &reward_amount);
         }
     }
 
-    fn calculate_rewards(&self, staking_position: &StakingPosition<Self::Api>) -> BigUint {
+    fn calculate_rewards(&self, staking_amount: &BigUint, staking_block: &u64) -> BigUint {
         let current_block = self.blockchain().get_block_nonce();
-        if current_block <= staking_position.last_action_block {
+        if current_block <= *staking_block {
             return BigUint::zero();
         }
 
         let base_distribution = self.base_distribution().get();
-        let block_diff = current_block - staking_position.last_action_block;
-				let time_diff = block_diff * 6;
-				let second_per_year = BLOCKS_IN_YEAR * 6;
+        let block_diff = current_block - *staking_block;
+        let time_diff = block_diff * 6;
+        let second_per_year = BLOCKS_IN_YEAR * 6;
 
-        &staking_position.stake_amount * base_distribution / MAX_PRECISION * time_diff / second_per_year
+        staking_amount * base_distribution / MAX_PRECISION * time_diff / second_per_year
     }
 
     #[view(calculateRewardsForUser)]
     fn calculate_rewards_for_user(&self, addr: ManagedAddress) -> BigUint {
-        let staking_pos = self.staking_position(&addr).get();
-        self.calculate_rewards(&staking_pos)
+        let staking_amount = self.staking_amount(&addr).get();
+        let staking_block = self.staking_block(&addr).get();
+        self.calculate_rewards(&staking_amount, &staking_block)
     }
 
     #[view(getStakedAddresses)]
     #[storage_mapper("stakedAddresses")]
     fn staked_addresses(&self) -> UnorderedSetMapper<ManagedAddress>;
 
-    #[view(getStakingPosition)]
-    #[storage_mapper("stakingPosition")]
-    fn staking_position(
-        &self,
-        addr: &ManagedAddress,
-    ) -> SingleValueMapper<StakingPosition<Self::Api>>;
+    // #[view(getStakingPosition)]
+    // #[storage_mapper("stakingPosition")]
+    // fn staking_position(
+    //     &self,
+    //     addr: &ManagedAddress,
+    // ) -> SingleValueMapper<StakingPosition<Self::Api>>;
+
+    #[view(getStakingAmount)]
+    #[storage_mapper("stakingAmount")]
+    fn staking_amount(&self, addr: &ManagedAddress) -> SingleValueMapper<BigUint>;
+
+    #[view(getStakingBlock)]
+    #[storage_mapper("stakingBlock")]
+    fn staking_block(&self, addr: &ManagedAddress) -> SingleValueMapper<u64>;
 
     #[view(getBaseDistribution)]
     #[storage_mapper("baseDistribution")]
